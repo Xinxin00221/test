52871ec7fe625039a07d5056f557bacbb9608177
Xinxin00221 <164991708+Xinxin00221@users.noreply.github.com>
2024-03-26 15:50:26 +0800

    Create json.py

---
+++ json.py
@@ -0,0 +1,17 @@
+'''
+JSON 是纯文本
+JSON 具有"自我描述性"（人类可读）
+JSON 具有层级结构（值中存在值）
+JSON 可通过 JavaScript 进行解析
+JSON 数据可使用 AJAX 进行传输
+'''
+
+# json 示例
+
+{
+    "sites":[
+        {"name": "菜鸟教程", "url": "www.runood.com"},
+        {"name": "google", "url": "www.google.com"},
+        {"name": "微博", "url": "www.weibo.com"}
+    ]
+}
